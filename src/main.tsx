a5e26a8b07ebdc9f8cab4b6a7be82c296386e859
ryantetro <Ryantetro@gmail.com>
2025-07-23 10:55:34 -0600

    Add ScrollToTop component in main.tsx for improved navigation experience

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import ScrollToTop from './components/ScrollToTop'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
       <Analytics />
     </BrowserRouter>
